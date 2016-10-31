e97510284079660969ae8eb61f287f7b7a989745
codeunlimited <vedipen@gmail.com>
2016-10-31 14:30:10 +0530

    DataBase URl

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
 //login details
 //remote login details
-$host = 'fdb6.your-hosting.net';
+$host = 'us-cdbr-iron-east-04.cleardb.net/heroku_61d99bfa265282c';
 $dbname = '2227060_bookdb';
-$user = '2227060_bookdb';
-$pwd = 'Codecell007!'; 
+$user = 'bf48dddc121d4f';
+$pwd = '64a9add4'; 
 
 
 // localhost login details
